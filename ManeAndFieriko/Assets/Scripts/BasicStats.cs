c2f35bc100550f9415a31a42c1a0fa0f48d202e9
Fieriko <111154099+Fieriko@users.noreply.github.com>
2023-03-01 12:10:21 +0000

    damage script

--- ManeAndFieriko/Assets/Scripts/BasicStats.cs
+++ ManeAndFieriko/Assets/Scripts/BasicStats.cs
@@ -4,6 +4,6 @@ using UnityEngine;
 
 public class BasicStats : MonoBehaviour
 {
-    public int hp,MeleeDamage;
+    public int hp;
     public float Speed;
 }
--- ManeAndFieriko/Assets/Scripts/Damage.cs
+++ ManeAndFieriko/Assets/Scripts/Damage.cs
@@ -5,4 +5,11 @@ using UnityEngine;
 public class Damage : MonoBehaviour
 {
     public int PhysicalDamage, FireDamage, IceDamge, ShockDamage,HolyDamage,PoisonDamage,DeathyDamage;
+
+    public void Hit(GameObject HitTarget)
+    {
+        int OverallDamage = PhysicalDamage + FireDamage+ IceDamge+ ShockDamage+ HolyDamage+ PoisonDamage+ DeathyDamage;
+        int targerHp = HitTarget.GetComponent<BasicStats>().hp;
+        targerHp = targerHp - OverallDamage;
+    }
 }
